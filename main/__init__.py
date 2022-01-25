0c803c8aabc32382536e2248c917513b873e3ebc
Victor_Bogdan <victor199704@gmail.com>
2022-01-25 18:44:21 +0200

    Added __init__.py to main for package conversion

---
+++ main/__init__.py
@@ -0,0 +1,5 @@
+from main_aucs import *
+from main_degree import *
+from main_harmonic import *
+from main_katz import *
+from main_subgraph import *
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='layer_centrality',
     version='0.1.0',
-    packages=[''],
+    packages=find_packages(),
     url='',
     license='Apache 2.0',
     author='Victor',
