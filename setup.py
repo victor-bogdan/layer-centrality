c322b641dc943d477126a4e6d4e0718fd0577ded
Victor_Bogdan <victor199704@gmail.com>
2022-01-25 18:35:37 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='layer_centrality',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='Apache 2.0',
+    author='Victor',
+    author_email='victor199704@gmail.com',
+    description='A package used for computing layer centrality in multilayered networks'
+)
