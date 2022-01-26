2858cc24d424cadb9b23a71b7cbe2493058d9e94
Victor_Bogdan <victor199704@gmail.com>
2022-01-26 19:59:28 +0200

    Added required external packages to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,16 @@ setup(
     license='Apache 2.0',
     author='Victor',
     author_email='victor199704@gmail.com',
-    description='A package used for computing layer centrality in multilayered networks'
+    description='A package used for computing layer centrality in multilayered networks',
+    install_requires=[
+        "uunet~=1.1.4",
+        "networkx~=2.6.3",
+        "matplotlib~=3.5.1",
+        "numpy~=1.22.1",
+        "seaborn~=0.11.2",
+        "pandas~=1.4.0",
+        "scikit-learn~=1.0.2",
+        "scipy~=1.7.3",
+        "setuptools~=57.0.0"
+    ]
 )
