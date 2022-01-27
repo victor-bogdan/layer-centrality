a9400a8df566db99a4ef69f768c9c3ee2c882ab0
Victor_Bogdan <victor199704@gmail.com>
2022-01-27 14:41:03 +0200

    Refactored LayerCentralityAnalyzer

--- layer_centrality/analyzers/layer_centrality_analyzers.py
+++ layer_centrality/analyzers/layer_centrality_analyzers.py
@@ -5,27 +5,28 @@ from layer_centrality.utils.centrality_helpers import DegreeCentralityHelper, Ha
 from layer_centrality.utils.centrality_measure import CentralityMeasure
 
 
-class CentralityAnalyzer:
+class LayerCentralityAnalyzer:
 
-    def __init__(self):
+    def __init__(self, dataset_helper):
+        self.__dataset_helper = dataset_helper
         self.__nodes_layer_centrality_dict = {}
         self.__results_data_frame = DataFrame.from_dict(self.__nodes_layer_centrality_dict)
 
-    def get_layer_centrality(self, centrality_measure, nx_layer_dict, node_list):
+    def get_layer_centrality(self, centrality_measure):
 
         if centrality_measure == CentralityMeasure.Degree:
-            centrality_helper = DegreeCentralityHelper(nx_layer_dict)
+            centrality_helper = DegreeCentralityHelper(self.__dataset_helper.get_nx_layer_dict())
         elif centrality_measure == CentralityMeasure.Harmonic:
-            centrality_helper = HarmonicCentralityHelper(nx_layer_dict)
+            centrality_helper = HarmonicCentralityHelper(self.__dataset_helper.get_nx_layer_dict())
         elif centrality_measure == CentralityMeasure.Katz:
-            centrality_helper = KatzCentralityHelper(nx_layer_dict)
+            centrality_helper = KatzCentralityHelper(self.__dataset_helper.get_nx_layer_dict())
         elif centrality_measure == CentralityMeasure.Subgraph:
-            centrality_helper = SubgraphCentralityHelper(nx_layer_dict)
+            centrality_helper = SubgraphCentralityHelper(self.__dataset_helper.get_nx_layer_dict())
         else:
             return self.__results_data_frame
 
         self.__nodes_layer_centrality_dict = compute_multinet_layer_centrality(
-            node_list, nx_layer_dict, centrality_helper)
+            self.__dataset_helper.get_nx_layer_dict(), self.__dataset_helper.get_node_list(), centrality_helper)
 
         self.__results_data_frame = DataFrame.from_dict(self.__nodes_layer_centrality_dict).T.sort_index(axis=1)
 
