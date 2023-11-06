27331f7bd152ba40ea498f2943091fdaf372e1a5
Victor Bogdan <victor199704@gmail.com>
2023-11-06 21:42:40 +0200

    Updated project version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='layer-centrality',
-    version='0.1.2',
+    version='1.0.0',
     packages=find_packages(),
     url='',
     license='Apache 2.0',
